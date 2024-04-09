9bf66b72c4bee72e26d0e6c1a944cd032c74ec38
Damelia Coleman <dameliac@outlook.com>
2024-04-09 15:10:15 -0500

    current version

--- RoleManagement.php
+++ RoleManagement.php
@@ -1,12 +1,9 @@
 <?php
 session_start();
-include_once "RoleManagementData.php";
-$mysqli = new mysqli("localhost", "sys", "laundry", "138users", 3307);
+include "../COMP2171-Group-3/RoleManagementData.php";
+require_once ("../COMP2171-Group-3/storage/sql_connect.php");
 
 
-if ($mysqli->connect_error){
-    die("Connection failed: " . $mysqli->connect_error);
-}
 
 $username = $_SESSION['userName'];
 //get the usertype as well as firstname and lastname of current user
@@ -18,18 +15,27 @@ $mysqli->close();
 
 <!--based on usertype the menu is loaded with different functions to be used by each user-->
 <img src="img\closeButton.png" alt="Close Button" id="close">
-<img src="img\profile.svg" alt="profile pic" id="profile">
+<img src="../COMP2171-Group-3/img/admin.png" alt="profile pic" id="profile">
 <p><?=$userInfo['first'] . " " . $userInfo['last']?></p>
 <?php if($userInfo['type']=="resident"): ?>
-    <div class="sideLinks"><a href="MachineBooking.php">Reservation Schedule</a></div>
-    <div class="sideLinks"><a href="WaitlistDisplay.php">Waitlist</a></div>
+    <div class="sideLinks"><a href="../COMP2171-Group-3/MachineBooking.php">Reservation Schedule</a></div>
+    <div class="sideLinks"><a href="../COMP2171-Group-3/WaitlistDisplay.php">Waitlist</a></div>
     <div class="sideLinks"><a href="TicketGenerator.php">Ticket View</a></div>
     <div class="sideLinks"><a href="MaintenanceRequest.php">Maintenance Request</a></div>
     <div class="sideLinks"><a href="BookingCancellation.php">Cancel Reservation</a></div>
+    <div class="sideLinks"><a href="">Forum</a></div>
+    <div class="sideLinks"><a href="../COMP2171-Group-3/profile.html">View Profile</a></div>
+    <hr style="width:97%;text-align:left;margin-left:0; height:0.1px; background-color:#7e7e7e; margin-bottom:0">
+    <a href="../COMP2171-Group-3/index.php"> <button> Sign Out</button></a>
 <?php elseif($userInfo['type']=="staff"):?>
-    <div class="sideLinks"><a href="TicketGenerator.php">Ticket Overview</a></div>
-    <div class="sideLinks"><a href="adminM.php">Maintenance Report</a></div>
-<?php else:?>
-    <div class="sideLinks"><a href="MachineRequest.php">Request Overview</a></div>
-    <div class="sideLinks"><a href="MachineStatusUpdate.php">Machine Statuses</a></div>
+    <div class="sideLinks"><a href="../COMP2171-Group-3/base.html">Dashboard</a></div>
+    <div class="sideLinks"><a href="../COMP2171-Group-3/profile.html">View Profile</a></div>
+    <div class="sideLinks"><a href="">Forum</a></div>
+    <div class="sideLinks"><a href="../COMP2171-Group-3/MachineStatusUpdate.php">Machine Status</a></div>
+    <div class ="sideLinks"><a href="../COMP2171-Group-3/Ticket Database.php">Ticket Database</a></div>
+    <div class="sideLinks"><a href="">Laundry Inventory</a></div>
+    <div class="sideLinks"><a href="../COMP2171-Group-3/MaintenanceRequestView.php">Request Overview</a></div>
+    <hr style="width:97%;text-align:left;margin-left:0; height:0.1px; background-color:#7e7e7e; margin-bottom:0">
+    <a href="../COMP2171-Group-3/index.php"> <button> Sign Out</button></a>
+
 <?php endif;?>
